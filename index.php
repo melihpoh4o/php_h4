60ebe2d4f95bcd82571cd3f76478de59b843ba2d
Melih <mredzhebov@roc-dev.com>
2019-04-03 09:23:31 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Melih
+ * Date: 3-4-2019
+ * Time: 09:12
+ */
+function telop ($a, $b) {
+    $c = $a + $b;
+    return $a. " + " .$b. " = " .$c;
+}
+
+echo telop(53434, 213123);
